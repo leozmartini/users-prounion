4540e82ccbfafeb239e6a529ca71513281ee9143
demar-nbk <demartinileo@hotmail.com>
2024-07-16 01:19:43 -0300

    feat: getUsersByID

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -9,3 +9,16 @@ export const getUsers = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const getUserByID = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const user = await userRepository.findOneBy({ id });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/users";
+import { getUserByID, getUsers } from "../controllers/users";
 
 const router = Router();
 
 router.get("/", getUsers);
+router.get("/:id", getUserByID);
 
 export default router;
