080ff29b0f037b6ce98be5f2ab3e197d15bec542
demar-nbk <demartinileo@hotmail.com>
2024-07-18 00:12:46 -0300

    refactor: change typeorm to methods using sql query

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,21 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-import { IsEmail, IsNotEmpty } from "class-validator";
-
-@Entity("users")
-export class User {
-  @PrimaryGeneratedColumn()
+export interface User {
   id: string;
-
-  @Column()
-  @IsNotEmpty({ message: "Name is required" })
   name: string;
-
-  @Column()
-  @IsNotEmpty({ message: "Email is required" })
-  @IsEmail({}, { message: "Invalid email" })
   email: string;
-
-  @Column()
-  @IsNotEmpty({ message: "Password is required" })
   password: string;
 }
